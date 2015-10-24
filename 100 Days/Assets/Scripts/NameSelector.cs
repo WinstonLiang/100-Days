78b6192ef4f63b2e58725210733b4d640728a519
Alex561 <alexchapp@yahoo.com>
2015-10-24 14:34:08 -0700

    Personal test scene and code names

--- 100 Days/Assets/Scripts/NameSelector.cs
+++ 100 Days/Assets/Scripts/NameSelector.cs
@@ -7,6 +7,7 @@ public class NameSelector : MonoBehaviour {
     string[] guys= {"Alex","Christopher","David","Leon","William","Winston","Aaron","Alan","James","Daivs","Arnold","Victor","Chirs","Jake","George","Mark","Michael","Rodney","Jack"};
     string[] girls = { "Tiffany", "Beth", "Alice", "Rhonda", "Tina", "Sarah","Chirstine","Kathy","Summer","Mary","Dawn","Wendy","Piper","Hillary"};
     string[] last = { "Chapp", "Walker", "Nguyen", "Perez","Liang","Loera","Birch","Gibson" ,"Wong","Walker"};
+    string[] codeName = { "Bob The Builder", "Super Spaceman", "Lone Wanderer" };
     string firstName;
     string lastName;
     public string name;
@@ -17,7 +18,7 @@ public class NameSelector : MonoBehaviour {
         if (gender == "girl"){
         firstName = girls[Random.Range(0, guys.Length)];}
         lastName = last[Random.Range(0, last.Length)];
-        name = (firstName +  ' ' + lastName);
+        name = codeName[Random.Range(0, codeName.Length)];
 	}
 	
 }
