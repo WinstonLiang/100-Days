4ba65f90d3c0696979b9a61b3995a8d4fc3b9f92
Alex561 <alexchapp@yahoo.com>
2015-10-24 15:15:16 -0700

    Game manager
    Press E to end turn to test

---
+++ 100 Days/Assets/Scripts/GameManegers.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManegers : MonoBehaviour {
+
+    public int days;
+    public int Hour;
+    public int r1;
+    public int r2;
+    public bool DayTime;
+	// Use this for initialization
+	void Update () {
+            if ( Input.GetKeyDown(KeyCode.E) == true )
+            {
+                EndTurn();
+            }
+            if ( Hour ==18 || Hour == 0)
+            {
+                DayTime = false;
+            }
+            if (Hour == 6 || Hour ==12)
+            {
+                DayTime = true;
+            }
+	}
+
+    void EndTurn()
+    {
+        Hour += 6;
+        if (Hour == 24)
+        {
+            Hour = 0;
+            days++;
+        }
+    }
+
+}
