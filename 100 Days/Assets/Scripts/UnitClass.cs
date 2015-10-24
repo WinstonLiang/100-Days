efa090454d8636a8502362681186e7638b58068d
Hyperlazydude <AnimeAzn101@gmail.com>
2015-10-24 15:46:04 -0700

    Update Class

--- 100 Days/Assets/Scripts/UnitClass.cs
+++ 100 Days/Assets/Scripts/UnitClass.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestScript : MonoBehaviour {
+public class TestScript : MonoBehaviour 
+{
+
+    public string firstName;
+    public string lastName;
+    public int level = 1;
+    public int health = 100;
+    public int att = 5;
+    public int def = 5;
+    public int speed =10;
+    public string ability;
+    public int crit = 1;
+    public int dogde = 1;
+    public int substat = 0;
+    public bool deadFlag = false;
+    public int classType = 0;
+
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+
+    void changeStat(int statChange, ref int orginalStat)
+    {
+        orginalStat = orginalStat - statChange;
+    }
+	void Update () 
+    {
+	   
 	}
 }
