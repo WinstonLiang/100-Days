7614d73acf4844d97e89c7b330846aac271cf5cc
L <lmatsuki87@gmail.com>
2015-10-24 17:15:08 -0700

    Battle Class

---
+++ 100 Days/Assets/Scripts/Battle.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battle : MonoBehaviour {
+
+    public UnitManager unitManager; // Retrieve squad information
+    public GameManegers gameManager; // Retrieve day information
+    public bool day;
+    public bool paused;
+    public bool playerParticipate;
+    private bool playerWon;
+    private bool enemyWon;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerWon = false;
+        enemyWon = false;
+        paused = false;
+        playerParticipate = true; // Set to true for testing
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(!playerWon && !enemyWon)
+        {
+            checkPlayerUnits();
+            checkEnemyUnits();
+
+            checkDeadUnits();
+            tickDown();
+        }
+	}
+
+    // Check if any player units are at tick 0
+    void checkPlayerUnits()
+    {
+
+    }
+
+    // Check if any enemy units are at tick 0
+    void checkEnemyUnits()
+    {
+
+    }
+
+    // Check if all player units or all enemy units are dead
+    void checkDeadUnits()
+    {
+        enemyWon = playersUnitsDead();
+        playerWon = enemyUnitsDead();
+    }
+
+    // Check if every player unit is dead
+    bool playersUnitsDead()
+    {
+        return false;
+    }
+
+    // Check if every enemy unit is dead
+    bool enemyUnitsDead()
+    {
+        return false;
+    }
+
+    // Decrement the tick for every unit
+    void tickDown()
+    {
+        // Decrement the tick for attacks
+
+        // Decrement the tick for abilities
+    }
+}
