88b9c34b3deb0a04a9d2b07cb91f39ea0a9a1009
Winston Liang <winstoyl@uci.edu>
2015-10-31 15:26:41 -0700

    Made Test Scene
    tested scene persistence

---
+++ 100 Days/Assets/Scripts/SceneTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTest : MonoBehaviour {
+     public int count = 0;
+
+	// Use this for initialization
+	void Start () {
+          print("start");
+          if (Application.loadedLevelName == "TestScene")
+          {
+               Application.LoadLevel("WinstonTest");
+          }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+          count++;
+          print(count);
+          if (count > 1000)
+          {
+               Application.LoadLevel("TestScene");
+          }
+	}
+}
