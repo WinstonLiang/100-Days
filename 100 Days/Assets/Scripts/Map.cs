36aa5bdac2b9fb082e7b20124895994a1b5940dc
WinstonLiang <winstoyl@uci.edu>
2016-01-23 18:28:07 -0800

    Made a Hexagon Map Tile creator

---
+++ 100 Days/Assets/Scripts/Map.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class tile
+{
+     //parameter should also include a representation of a group of enemies
+     public Vector3 tilePosition;
+     public int terrain = 0;
+
+     private bool visilibity;
+     private bool battle;
+     private bool squad = false;
+}
+
+
+
+public class Map : MonoBehaviour {
+
+     public GameObject maptile;
+     public List<tile> tiles;
+
+	// Use this for initialization
+	void Start () {
+          foreach (tile T in tiles)
+          {
+               Instantiate(maptile, T.tilePosition, Quaternion.identity);
+          }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
