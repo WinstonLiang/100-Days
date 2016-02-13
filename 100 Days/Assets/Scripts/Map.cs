56be47d65ae2cf123ea9315a590ccebe8a982cf9
WinstonLiang <winstoyl@uci.edu>
2016-02-13 14:27:58 -0800

    camera movement script added, map script generation being changed

--- 100 Days/Assets/Scripts/Map.cs
+++ 100 Days/Assets/Scripts/Map.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
-using System.Linq;
 
 [System.Serializable]
 public class tile
@@ -12,50 +10,48 @@ public class tile
      public Vector3 tilePosition;
      public int terrain = 0;
 
-     private bool visilibity;
-     private bool battle;
-     private bool squad = false;
+     public bool visilibity;
+     public bool battle;
+     public bool squad = false;
 }
 
 
 
-public class Map : MonoBehaviour {
+public class Map : MonoBehaviour
+{
 
      public GameObject maptile;
-     public string mapFile;
+     public int size;
      public float originX;
      public float originY;
      private List<tile> tiles;
 
-          
-	// Use this for initialization
-	void Start () {
+
+     // Use this for initialization
+     void Start()
+     {
           tiles = new List<tile>();
-          string line;
-          using (StreamReader file = new StreamReader(Application.dataPath + "/maps/" + mapFile)){
-               while ((line = file.ReadLine()) != null)
+          for (float x = 0; x <= size; x++)
+          {
+               for (float y = 0; y <= size; y++)
                {
-                    string[] mapData = line.Split(';');
                     tile newTile = new tile();
-                    Vector3 pos = new Vector3(float.Parse(mapData[0])+originX,
-                         -(float.Parse(mapData[1])+originY+(float.Parse(mapData[0])%2)*.5f),0);
+                    Vector3 pos = new Vector3(x + originX, -(y + originY) + (x % 2) * .5f, 0);
                     newTile.tilePosition = pos;
                     tiles.Add(newTile);
                }
-               
           }
-
-
 
           foreach (tile T in tiles)
           {
                Instantiate(maptile, T.tilePosition, Quaternion.identity);
           }
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }
 }
